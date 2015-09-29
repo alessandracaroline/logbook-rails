814acfb8c86e4d408e18761d48b05176f5db4f9a
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-09-29 14:47:24 -0700

    add trip show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,38 @@
 class TripsController < ApplicationController
 
+  def index
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
   def new
-    # @trip = Trip.new
+    @trip = Trip.new
+  end
+
+  def edit
   end
 
   def create
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      redirect_to @trip
+    else
+      render 'new'
+    end
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def trip_params
+      params.require(:trip).permit(:title, :skipper_name, :boat_name, :boat_type, :start_date, :end_date, :origin, :destination)
+    end
 
 end
