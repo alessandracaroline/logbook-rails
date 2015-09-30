2da901fa86970a8c32b7efa59c0b2ff26b112599
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-09-30 11:04:43 -0700

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :trips do
     resources :logs
   end
+
+  root 'trips#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
