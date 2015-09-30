b1d61a6516410473f1a0d356938a431d94966f52
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-09-29 19:21:39 -0700

    make log creation work

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -8,4 +8,22 @@ class LogsController < ApplicationController
     @log = Log.new
   end
 
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @log = @trip.logs.new(log_params)
+
+    p @log
+
+    if @log.save
+      redirect_to trip_path(@trip)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def log_params
+      params.require(:log).permit(:latitude_hemisphere, :latitude_degrees, :latitude_minutes, :latitude_seconds, :longitude_hemisphere, :longitude_degrees, :longitude_minutes, :longitude_seconds, :nautical_miles, :incidents)
+    end
+
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,8 +1,8 @@
 class Log < ActiveRecord::Base
   belongs_to :trip
   validates :latitude_hemisphere, :latitude_degrees, :latitude_minutes, :latitude_seconds, :longitude_hemisphere, :longitude_degrees, :longitude_minutes, :longitude_seconds, presence: true
-    validates :latitude_minutes, :latitude_seconds, :longitude_minutes, :longitude_seconds, numericality: {:greater_than_or_equal_to => 0, :less_than => 60 }
-    validates_numericality_of :latitude_degrees, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 90
-    validates_numericality_of :longitude_degrees, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 180
-    validates :latitude_degrees, :longitude_degrees, :latitude_minutes, :longitude_minutes, numericality: { only_integer: true }
+  validates :latitude_minutes, :latitude_seconds, :longitude_minutes, :longitude_seconds, numericality: {:greater_than_or_equal_to => 0, :less_than => 60 }
+  validates_numericality_of :latitude_degrees, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 90
+  validates_numericality_of :longitude_degrees, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 180
+  validates :latitude_degrees, :longitude_degrees, :latitude_minutes, :longitude_minutes, numericality: { only_integer: true }
 end
