ecbe849e005cc2937ebafd805e429fcbd5d123b4
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-10-09 17:47:21 -0700

    change

--- app/assets/javascripts/logs_show.js
+++ app/assets/javascripts/logs_show.js
@@ -1,5 +1,5 @@
 $(document).on("sailor:logs_show", function(){
-  // var map;
+  var map;
   initMap();
 });
 
@@ -7,16 +7,17 @@ function initMap() {
     var latitude = $('#log_latitude').val();
     var longitude = $('#log_longitude').val();
     var myLatLng = {lat: Number(latitude), lng: Number(longitude)};
+    var mapDiv = document.getElementById('map');
+    var mapOptions = {
+      center: myLatLng,
+      zoom: 11
+    }
 
     console.log(myLatLng)
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 11
-    });
+    map = new google.maps.Map(mapDiv, mapOptions);
 
     console.log(map)
-    $('#map').append(map)
 
     var marker = new google.maps.Marker({
       position: myLatLng,
@@ -24,3 +25,14 @@ function initMap() {
       title: 'Your log!'
     });
   }
+
+
+  // google.maps.event.addDomListener(window, 'load', function() {
+  //   var canvas = document.getElementById("map-canvas");
+  //   var mapOptions = {
+  //     center: new google.maps.LatLng(40.714352, -74.005973),
+  //     zoom:   7
+  //   };
+
+  //   var map = new google.maps.Map(canvas, mapOptions);
+  // });
